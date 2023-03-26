dc3c003bdee4566d155c2a62f2a7a30904dd51ef
kinurra <59222354+kinurra@users.noreply.github.com>
2023-03-27 05:52:11 +0700

    Update app.js

--- node/src/app.js
+++ node/src/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
 
-const PORT = process.env.PORT || 4001;
+const PORT = process.env.PORT || 80;
 
 const app = express();
 const httpServer = http.createServer(app);
